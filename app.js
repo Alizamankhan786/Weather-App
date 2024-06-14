d213b808ef3b5bd52d089651b55e9c379d87e879
Alizamankhan786 <az030366@gmail.com>
2024-06-13 22:45:11 -0700

    index file created

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+// API RENDER
+
+
+// console.log(`WEATHER APP`);
+
+
+
+// 0340d1177362401997950731241406    WEATHER API KEY
+
+
+// http://api.weatherapi.com/v1/current.json?key=0340d1177362401997950731241406&q=London&aqi=no   
+
+
+
+const form = document.querySelector(`#form`);
+const div = document.querySelector(`#cd`);
+
+
+form.addEventListener()
+
+
+
+axios(`http://api.weatherapi.com/v1/current.json?key=0340d1177362401997950731241406&q=London&aqi=no `);
