cd1b8b132c9eb77d3fc3659b888f846ebb9f8137
Alizamankhan786 <az030366@gmail.com>
2024-06-19 15:56:23 -0700

    index file created

--- app.js
+++ app.js
@@ -15,11 +15,47 @@
 
 
 const form = document.querySelector(`#form`);
-const div = document.querySelector(`#cd`);
+const city = document.querySelector(`#city`);
+const card = document.querySelector(`#card`);
 
 
-form.addEventListener()
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    axios(
+    `http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no`
+)
+
+.then((res) => {
+
+    console.log(res.data);
+
+    card.innerHTML = `
+
+     <div class="header">
+            <h2>${res.data.location.name}</h2>
+        </div>
+    <div class="content">
+        <div class="weather-icon">
+            <img src="${res.data.current.condition.icon}" alt="Weather Icon">
+              </div>
+              <div class="weather-info">
+              <p>${res.data.location.localtime} ,${res.data.location.country}</p>
+                <h3>${res.data.current.temp_c}°C</h3>
+              </div>
+            </div>
+            <div class="footer">
+              <h4>${res.data.current.condition.text}</h4>
+            </div>
+    ` ;
+})
+.catch((err) => {
+    console.log("error===>", err);
+});
+
+});
 
 
 
-axios(`http://api.weatherapi.com/v1/current.json?key=0340d1177362401997950731241406&q=London&aqi=no `);
+
+
+// axios(`http://api.weatherapi.com/v1/current.json?key=0340d1177362401997950731241406&q=London&aqi=no `);
